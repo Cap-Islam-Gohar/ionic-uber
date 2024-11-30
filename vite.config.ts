c8ed8d43bbd1be66a2288185b99055cff92d0f4d
IslamGohar <cap.islamadel@gmail.com>
2024-11-30 16:54:14 +0200

    Adding screenshoots images

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,64 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy(),
-    VitePWA({ registerType: 'autoUpdate' })
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
+    plugins: [
+        react(),
+        legacy(),
+        VitePWA({registerType: 'autoUpdate'})
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+    }
 })
+
+
+// const pwaConfig = {
+//     manifest: {
+//         "start_url": "/",
+//         "theme_color": "#000000",
+//         "background_color": "#000000",
+//         "icons": [
+//             {
+//                 "purpose": "maskable",
+//                 "sizes": "512x512",
+//                 "src": "icon512_maskable.png",
+//                 "type": "image/png"
+//             },
+//             {
+//                 "purpose": "any",
+//                 "sizes": "512x512",
+//                 "src": "icon512_rounded.png",
+//                 "type": "image/png"
+//             }
+//         ],
+//         "screenshots": [
+//             { "src": "/screenshots/Untitled.png", "type": "image/png", "sizes": "540x720", "form_factor": "narrow" },
+//             { "src": "/screenshots/Untitled-2.png", "type": "image/png", "sizes": "540x720", "form_factor": "narrow" },
+//             { "src": "/screenshots/Untitled-3.png", "type": "image/png", "sizes": "540x720", "form_factor": "narrow" }
+
+//         ],
+//         "orientation": "any",
+//         "display": "standalone",
+//         "lang": "en-US",
+//         "name": "Ubur",
+//         "short_name": "Ubur",
+//         "description": "Ubur - Plan your ride",
+//         "id": "client-app"
+//     },
+//     workbox: {
+//         runtimeCaching: [{
+//             urlPattern: ({ url }) => {
+//                 return url.pathname.startsWith('/api')
+//             },
+//             handler: "cacheFirst" as const,
+//             options: {
+//                 cacheName: "api-cache",
+//                 cacheableResponse: {
+//                     statuses: [0, 200]
+//                 }
+//             }
+//         }]
+//     }
+// }
